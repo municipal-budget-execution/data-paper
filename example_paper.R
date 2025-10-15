59c028706f1f0303294d528e9ec14d635053514f
Thiago Scot <31219574+thiagoscot@users.noreply.github.com>
2025-10-14 21:43:44 -0400

    Update example_paper.R

--- example_paper.R
+++ example_paper.R
@@ -16,6 +16,10 @@
 
 #write.csv(data, paste0(input, "data_histogram_licitacao.csv"), row.names = FALSE)
 
+input = '/Users/tscot/Downloads/RR_BRA_2023_19-v01 2/data-paper/Data/Raw'
+graph_output = '/Users/tscot/Dropbox/Aplicativos/Overleaf/MiDES - New Data and Facts from Brazil/figures'
+
+
 # Open file
 file_path <- file.path(input, "data_histogram_licitacao.csv")
 data <- read.csv(file_path)
@@ -24,9 +28,16 @@ data <- read.csv(file_path)
 data %>% filter(count > 50) %>% 
   ggplot() + 
   geom_histogram(aes(x = share_discretion), bins = 100,
-                 color="black", fill="#02075d") + 
+                 color = "#0D3446",
+                 fill = "#1A476F") + 
   xlab("Share of non-competitive tenders") +
-  theme_classic()
+  ylab("Number") +
+  theme_classic() +
+  theme(
+    strip.text = element_text(size = 7, face = "bold"),
+    axis.title = element_text(size = 11),
+    axis.text=element_text(size=10)
+  )
 ggsave(filename = file.path(graph_output, "histogram_noncompetitive.png"),
        width = 8,
        height = 5)
