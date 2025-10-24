f42be7e4157d87f56c0b37341342fc91497d2ef3
Thiago Scot <31219574+thiagoscot@users.noreply.github.com>
2025-10-24 15:47:34 -0400

    Update Extract_Items_Mides.R

--- Extract_Items_Mides.R
+++ Extract_Items_Mides.R
@@ -17,11 +17,11 @@ set_billing_id("projetobd-302617")
 query = "SELECT id_licitacao_bd, id_municipio, sigla_uf, descricao, 
 CASE 
   WHEN sigla_uf != 'PR' THEN valor_total
-  ELSE valor_vencedor
+  ELSE quantidade_proposta*valor_vencedor
 END as valor_item
 FROM `basedosdados.world_wb_mides.licitacao_item`
 WHERE ano = 2021 AND id_licitacao_bd IS NOT NULL "
 items_query = read_sql(query)
 
-items_query = items_query[!is.na(valor_item)]
-fwrite(items_query,  '/Users/tscot/Dropbox/MiDES-data-paper-replication/Data/Raw/mides_2021_items.csv')
+items_query = setDT(items_query)[!is.na(valor_item)]
+fwrite(items_query,  '/Users/tscot/Dropbox/MiDES-data-paper-replication/Data/Intermediate/mides_2021_items.csv')
