83ab47b4a00cef264108f734041471cbae3a4076
Thiago Scot <31219574+thiagoscot@users.noreply.github.com>
2025-10-28 12:07:15 -0400

    Update Deviations_SICONFI_Compras.R

--- Deviations_SICONFI_Compras.R
+++ Deviations_SICONFI_Compras.R
@@ -7,66 +7,66 @@ library(data.table)
 library(tibble)
 library(patchwork)
 
-path_file = '/Users/tscot/Dropbox/WBER_RR/Data/Compare_Siconfi_Tender'
+path_file = '/Users/tscot/Dropbox/MiDES-data-paper-replication/Data/Intermediate'
 path_figures = '/Users/tscot/Dropbox/Aplicativos/Overleaf/MiDES - New Data and Facts from Brazil/figures'
   
 #Set user's BigQuery billing ID
 # set_billing_id("projetobd-302617")
 # 
- query = "WITH commitment AS (
-   SELECT
-   e.ano
-   ,e.sigla_uf
-   ,e.id_municipio
-   ,ROUND(SUM(CASE WHEN SUBSTR (e.elemento_despesa, 5,2) IN ('30','32', '33','35', '36', '37', '38', '39', '51', '52') THEN valor_final END),2) AS total_compras_empenho
-   ,ROUND(SUM(CASE WHEN SUBSTR (e.elemento_despesa, 5,2) IN ('30','32', '33','35', '36', '37', '38', '39', '51', '52') THEN valor_inicial END),2) AS total_compras_empenho_inicial
-   ,ROUND(SUM(valor_final),2) AS total_empenho
- FROM basedosdados.world_wb_mides.empenho e
- WHERE e.sigla_uf IN ('PE','PB','MG','CE','PR', 'RS') AND e.ano >= 2010
- GROUP BY 1,2,3
- )
-
- ,procurement AS (
-SELECT
-  p.ano
-  ,p.sigla_uf
-  ,p.id_municipio
-  ,ROUND(SUM(p.valor_orcamento),2) AS total_compras_orcamento
-  ,ROUND(SUM(p.valor_corrigido),2) AS total_compras_licitacao
-FROM basedosdados.world_wb_mides.licitacao p
-WHERE situacao = '1' OR situacao IS NULL
-GROUP BY 1,2,3
-)
-
-,item AS (
-SELECT
-  i.ano
-  ,i.sigla_uf
-  ,i.id_municipio
-  ,ROUND(SUM(i.valor_total),2) AS total_valor_items
-  ,ROUND(SUM(i.valor_vencedor),2) AS total_valor_vencedor
-FROM basedosdados.world_wb_mides.licitacao_item i
-GROUP BY 1,2,3
-)
-
-SELECT
-  e.ano
-  ,e.sigla_uf
-  ,e.id_municipio
-  ,e.total_empenho
-  ,e.total_compras_empenho
-  ,e.total_compras_empenho_inicial
-  ,p.total_compras_orcamento
-  ,p.total_compras_licitacao
-  ,i.total_valor_items
-  ,i.total_valor_vencedor
-FROM commitment e
-LEFT JOIN procurement p ON e.ano = p.ano AND e.sigla_uf = p.sigla_uf AND e.id_municipio = p.id_municipio
-LEFT JOIN item i ON e.ano = i.ano AND e.sigla_uf = i.sigla_uf AND e.id_municipio = i.id_municipio
-ORDER BY 1,2,3"
-
-items_query = read_sql(query)
-fwrite(items_query,  '/Users/tscot/Dropbox/WBER_RR/Data/Compare_Siconfi_Tender/siconfi_compras.csv')
+#  query = "WITH commitment AS (
+#    SELECT
+#    e.ano
+#    ,e.sigla_uf
+#    ,e.id_municipio
+#    ,ROUND(SUM(CASE WHEN SUBSTR (e.elemento_despesa, 5,2) IN ('30','32', '33','35', '36', '37', '38', '39', '51', '52') THEN valor_final END),2) AS total_compras_empenho
+#    ,ROUND(SUM(CASE WHEN SUBSTR (e.elemento_despesa, 5,2) IN ('30','32', '33','35', '36', '37', '38', '39', '51', '52') THEN valor_inicial END),2) AS total_compras_empenho_inicial
+#    ,ROUND(SUM(valor_final),2) AS total_empenho
+#  FROM basedosdados.world_wb_mides.empenho e
+#  WHERE e.sigla_uf IN ('PE','PB','MG','CE','PR', 'RS') AND e.ano >= 2010
+#  GROUP BY 1,2,3
+#  )
+# 
+#  ,procurement AS (
+# SELECT
+#   p.ano
+#   ,p.sigla_uf
+#   ,p.id_municipio
+#   ,ROUND(SUM(p.valor_orcamento),2) AS total_compras_orcamento
+#   ,ROUND(SUM(p.valor_corrigido),2) AS total_compras_licitacao
+# FROM basedosdados.world_wb_mides.licitacao p
+# WHERE situacao = '1' OR situacao IS NULL
+# GROUP BY 1,2,3
+# )
+# 
+# ,item AS (
+# SELECT
+#   i.ano
+#   ,i.sigla_uf
+#   ,i.id_municipio
+#   ,ROUND(SUM(i.valor_total),2) AS total_valor_items
+#   ,ROUND(SUM(i.valor_vencedor),2) AS total_valor_vencedor
+# FROM basedosdados.world_wb_mides.licitacao_item i
+# GROUP BY 1,2,3
+# )
+# 
+# SELECT
+#   e.ano
+#   ,e.sigla_uf
+#   ,e.id_municipio
+#   ,e.total_empenho
+#   ,e.total_compras_empenho
+#   ,e.total_compras_empenho_inicial
+#   ,p.total_compras_orcamento
+#   ,p.total_compras_licitacao
+#   ,i.total_valor_items
+#   ,i.total_valor_vencedor
+# FROM commitment e
+# LEFT JOIN procurement p ON e.ano = p.ano AND e.sigla_uf = p.sigla_uf AND e.id_municipio = p.id_municipio
+# LEFT JOIN item i ON e.ano = i.ano AND e.sigla_uf = i.sigla_uf AND e.id_municipio = i.id_municipio
+# ORDER BY 1,2,3"
+# 
+# items_query = read_sql(query)
+# fwrite(items_query,  '/Users/tscot/Dropbox/WBER_RR/Data/Compare_Siconfi_Tender/siconfi_compras.csv')
 
 data = fread(paste0(path_file, '/siconfi_compras.csv'))
 
@@ -85,19 +85,28 @@ data = data %>%
 
 median_all = median(data$ratio_lic, na.rm = T)
 
-data %>% mutate(ratio_lic = pmin(ratio_lic, 200)) %>% ggplot() + 
+data %>% mutate(ratio_lic = pmin(ratio_lic, 300)) %>% ggplot() + 
   geom_histogram(aes(x = ratio_lic,
                  y = stat(width*density)), binwidth = 5, 
                  color = "#0D3446",
                  fill = "#1A476F", alpha = .5) + 
   geom_vline(xintercept = median_all, color = 'red', linetype = 'dashed', linewidth = 1) + 
-  theme_classic() + 
   geom_vline(xintercept = 0) +
   labs(
     x = "Deviation Tenders - Procurement Commitments",
     y = "Share"
-  ) 
-ggsave(filename = paste0(path_figures, '/deviations_procurement_all.png'))
+  )  +
+  theme_classic(base_size = 14) +   # increase base font size
+  theme(
+    axis.title = element_text(size = 14, face = "bold"),
+    axis.text = element_text(size = 13),
+    legend.position = "none"        # remove legend for size
+  )
+ggsave(filename = paste0(path_figures, '/deviations_procurement_all.png'),
+       width = 10,    # Increase the width
+       height = 5.625,    # Keep a standard height
+       units = "in",
+       dpi = 300)
 
 data %>%  mutate(ratio_lic = pmin(ratio_lic, 200)) %>% ggplot() + 
   geom_histogram(aes(x = ratio_lic,
@@ -110,14 +119,19 @@ data %>%  mutate(ratio_lic = pmin(ratio_lic, 200)) %>% ggplot() +
     x = "Deviation Tenders - Procurement Commitments",
     y = "Share"
   ) +
-  theme_classic() +
+  theme_classic(base_size = 14) +   # increase base font size
   theme(
-    strip.text = element_text(size = 7, face = "bold"),
-    axis.title = element_text(size = 10),
+    axis.title = element_text(size = 14, face = "bold"),
+    axis.text = element_text(size = 13),
+    strip.text = element_text(size = 9, face = "bold"),
     strip.background = element_blank()
   ) +
   geom_vline(aes(xintercept = median_state_lic), color = 'red', linetype = 'dashed')
-ggsave(filename = paste0(path_figures, '/deviations_procurement_states.png'))
+ggsave(filename = paste0(path_figures, '/deviations_procurement_states.png'),
+       width = 10,    # Increase the width
+       height = 5.625,    # Keep a standard height
+       units = "in",
+       dpi = 300)
 
 
 data %>% filter(total_compras_empenho > 1e6 & total_compras_licitacao > 1e6) %>%  
@@ -186,3 +200,30 @@ data %>% filter(ratio_lic < 200 & ratio_item < 200) %>% ggplot() +
     strip.background = element_blank()
   ) +
   geom_vline(aes(xintercept = median_state_lic), color = 'red', linetype = 'dashed')
+
+
+
+#Municipality level
+
+data[ano!= 2020 & ano != 2014, .(total_compras_licitacao = sum(total_compras_licitacao),
+         total_compras_empenho = sum(total_compras_empenho)), 
+     by = id_municipio] %>% 
+  mutate(ratio_lic  = 100*(total_compras_licitacao/total_compras_empenho-1)) %>% 
+  mutate(ratio_lic = pmin(ratio_lic, 300)) %>% 
+  ggplot() + 
+  geom_histogram(aes(x = ratio_lic,
+                     y = stat(width*density)), binwidth = 5, 
+                 color = "#0D3446",
+                 fill = "#1A476F", alpha = .5) + 
+  geom_vline(xintercept = median_all, color = 'red', linetype = 'dashed', linewidth = 1) + 
+  geom_vline(xintercept = 0) +
+  labs(
+    x = "Deviation Tenders - Procurement Commitments",
+    y = "Share"
+  )  +
+  theme_classic(base_size = 14) +   # increase base font size
+  theme(
+    axis.title = element_text(size = 14, face = "bold"),
+    axis.text = element_text(size = 13),
+    legend.position = "none"        # remove legend for size
+  )
